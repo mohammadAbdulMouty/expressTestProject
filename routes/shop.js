eff2b22953abb33cfe5b3796f28323c6f39cf370
advd-github <67094573+advd-github@users.noreply.github.com>
2021-04-12 01:05:03 +0300

    working Template engines pugs

--- routes/shop.js
+++ routes/shop.js
@@ -6,8 +6,10 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const adminData = require('./admin');
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  const products = adminData.products;
+res.render('shop',{prods:products,docTitle:'shop',path:"/"});
 });
 
 module.exports = router;
