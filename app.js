d1b33abba7cc7b33901ffe31731c01ecf2eafb44
advd-github <67094573+advd-github@users.noreply.github.com>
2021-04-14 02:02:19 +0300

    template engine using ejs

--- app.js
+++ app.js
@@ -5,13 +5,7 @@ const bodyParser = require("body-parser");
 const expressHbs = require('express-handlebars');
 
 const app = express();
-
-app.engine('hbs',expressHbs({
-  layoutsDir:'views/layouts/',
-  defaultLayout:'main-layout',
-  extname:'hbs'
-}))
-app.set("view engine", "hbs");
+app.set("view engine", "ejs");
 app.set("views", "views");
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
