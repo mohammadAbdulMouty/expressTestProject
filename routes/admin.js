8698ec42f9365d05b72c5eda1bde86ff18ca85ee
advd-github <67094573+advd-github@users.noreply.github.com>
2021-04-14 00:24:02 +0300

    add template engine using handlebars

--- routes/admin.js
+++ routes/admin.js
@@ -8,8 +8,11 @@ const router = express.Router();
 const products = [];
 // /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
-  res.render('add-product',{docTitle:'add product',path:'/admin/add-product'})
-  
+  res.render("add-product", {
+    docTitle: "add product",
+    path: "/admin/add-product",
+    productCSS:true,
+  });
 });
 
 // /admin/add-product => POST
--- routes/shop.js
+++ routes/shop.js
@@ -1,15 +1,23 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const rootDir = require('../util/path');
+const rootDir = require("../util/path");
 
 const router = express.Router();
 
-const adminData = require('./admin');
-router.get('/', (req, res, next) => {
+const adminData = require("./admin");
+router.get("/", (req, res, next) => {
   const products = adminData.products;
-res.render('shop',{prods:products,docTitle:'shop',path:"/"});
+  res.render("shop", {
+    prods: products,
+    docTitle: "shop",
+    path: "/",
+    hasProducts: products.length > 0,
+    activeShop:true,
+    productCSS:true,
+    // layout:false
+  });
 });
 
 module.exports = router;
